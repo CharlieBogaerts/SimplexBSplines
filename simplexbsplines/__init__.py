a5fe1e7ed8c96b9eaeced4234de652fa044a0707
Charlie Bogaerts <charlie.bogaerts@demcon.com>
2024-08-22 00:37:17 +0200

    still fighting with git

---
+++ simplexbsplines/__init__.py
@@ -0,0 +1,7 @@
+"""
+Explanation
+"""
+
+from simplexbsplines.ssmodel import SSModel
+from simplexbsplines.triangulation import Triangulation
+from simplexbsplines.storage import save_model, open_model
