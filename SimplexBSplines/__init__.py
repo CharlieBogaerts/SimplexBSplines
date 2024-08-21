e63f73ea1aad195f7df081e7fe4781a41287f551
Charlie Bogaerts <charlie.bogaerts@demcon.com>
2024-08-22 00:36:05 +0200

    delete first

--- SimplexBSplines/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Explanation
-"""
-
-from simplexbsplines.ssmodel import SSModel
-from simplexbsplines.triangulation import Triangulation
-from simplexbsplines.storage import save_model, open_model
